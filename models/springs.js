c371f93d8ad6b5f3a1b9544c5934b2fcc46faa8a
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-07-15 16:06:09 -0600

    inserted 4 springs

--- models/springs.js
+++ models/springs.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const springSchema = new mongoose.Schema({
 	name: String,
-	location: [String],
+	location: String,
 	temperature: Number,
 });
 
@@ -10,26 +10,3 @@ module.exports = mongoose.model('Spring', springSchema);
 
 
 
-// {
-// 	[
-// 	name: 'Radium',
-// 	location: 'British Columbia, Canada',
-// 	temperature: 104,
-// 	],
-// 	[
-// 	name: 'Bob Weir',
-// 	location: 'Kooskia, Idaho, USA',
-// 	temperature: 102,
-// 	],
-// 	[
-// 	name: 'Goldbug',
-// 	location: 'Salmon, Idaho, USA',
-// 	temperature: 103,
-// 	],
-// 	[
-// 	name: 'Siams',
-// 	location: 'Hot Springs, Montanta, USA'
-// 	temperature: 101,
-// 	]
-
-// }
---
+++ models/springsPopulate.js
@@ -0,0 +1,26 @@
+
+
+
+[
+	{
+	name: 'Radium',
+	location: 'British Columbia, Canada',
+	temperature: 104,
+	},
+	{
+	name: 'Bob Weir',
+	location: 'Kooskia, Idaho, USA',
+	temperature: 102,
+	},
+	{
+	name: 'Goldbug',
+	location: 'Salmon, Idaho, USA',
+	temperature: 103,
+	},
+	{
+	name: 'Siams',
+	location: 'Hot Springs, Montanta, USA'
+	temperature: 101,
+	}
+
+]
