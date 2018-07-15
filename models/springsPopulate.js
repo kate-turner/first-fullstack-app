066fd779415a6776f5f13d382db909bb296d2b34
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-07-15 17:18:48 -0600

    index page running

--- models/springsPopulate.js
+++ models/springsPopulate.js
@@ -1,26 +1,27 @@
 
-
-
+const springsPopulate = 
 [
 	{
 	name: 'Radium',
 	location: 'British Columbia, Canada',
-	temperature: 104,
+	temperature: 104
 	},
 	{
 	name: 'Bob Weir',
 	location: 'Kooskia, Idaho, USA',
-	temperature: 102,
+	temperature: 102
 	},
 	{
 	name: 'Goldbug',
 	location: 'Salmon, Idaho, USA',
-	temperature: 103,
+	temperature: 103
 	},
 	{
 	name: 'Siams',
-	location: 'Hot Springs, Montanta, USA'
-	temperature: 101,
+	location: 'Hot Springs, Montanta, USA',
+	temperature: 101
 	}
 
-]
+];
+
+module.exports = springsPopulate;
--- server.js
+++ server.js
@@ -3,35 +3,17 @@ const app = express();
 
 require('./db/db');
 
-const Springs = require("./models/springs.js")
+// const Springs = require("./models/springs.js")
 
-Springs.collection.insertMany([
-	{
-	name: 'Radium',
-	location: 'British Columbia, Canada',
-	temperature: 104
-	},
-	{
-	name: 'Bob Weir',
-	location: 'Kooskia, Idaho, USA',
-	temperature: 102
-	},
-	{
-	name: 'Goldbug',
-	location: 'Salmon, Idaho, USA',
-	temperature: 103
-	},
-	{
-	name: 'Siams',
-	location: 'Hot Springs, Montanta, USA',
-	temperature: 101,
-	}], (err, resSprings) => {
-		if(err) {
-		console.log(err);
-		} else {
-		console.log(resSprings)
-		}
-	});
+const Springs = require("./models/springsPopulate.js")
+
+app.get('/springs', (req, res) => {
+	res.render("index.ejs", {
+		"springsList":Springs
+	})
+})
+
+
 
 
 
